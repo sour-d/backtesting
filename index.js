dc37f8b9a271296668d486809d4e6deb28a0ad0a
Sourav Das <sdas22@lenovo.com>
2023-01-01 12:13:28 +0530

    refactored index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+// Import starts
 const { parse } = require("./src/parser.js");
 const { Stock } = require("./src/Stock.js");
 const { FortyTwentyStrategy } = require("./src/strategy/FortyTwentyStrategy.js");
+const { MovingAverageStrategy } = require("./src/strategy/MovingAverageStrategy.js");
+// Import ends
 
-const stockSymbols = ["TCS", "NIFTY", "BAJFINANCE"];
 
-stockSymbols.forEach((symbol) => {
-  const { data } = parse(symbol);
-  const stock = new Stock(data, 40); //should start from 40th day otherwise data will be inaccurate
-  const strategy = new FortyTwentyStrategy(stock, 100000, 0.02);
+// Global flags
+const STRATEGY = FortyTwentyStrategy;
+const STOCKSYMBOLS = ["TCS", "NIFTY", "BAJFINANCE"];
+// Global flags
+
+
+// Main logic to get the result
+const getResult = (symbol) => {
+  const { data: stockData } = parse(symbol);
+  const stock = new Stock(stockData, 200); //should start from 40th day otherwise data will be inaccurate
+  const capital = 100000;
+  const riskFactor = 0.02;
+  const strategy = new STRATEGY(stock, capital, riskFactor);
 
   console.log(`Expectancy of ${symbol} is ${strategy.getExpectancy()}`);
-});
+};
+
+STOCKSYMBOLS.forEach(getResult);
