cb19332b4fd913fde34aab7d70f35f21a6aea754
sour-d <36912347+sour-d@users.noreply.github.com>
2023-06-18 19:00:40 +0530

    added function to download stock data based on symbol list

---
+++ src/downloadData.js
@@ -0,0 +1,53 @@
+const symbolList = require('./symbolList.json');
+const https = require('node:https');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const DIR_PATH = './data';
+
+const writeIntoFile = (filename, data) => {
+  fs.writeFileSync(filename, data, { flag: 'a', encoding: 'utf8' });
+};
+
+const onErrorReciveOf = (name) =>
+  (error) => {
+    console.error("Got error");
+    console.error("Name:", name);
+    console.error("Error is:", error);
+  };
+
+const onDataReciveOf = (name) =>
+  (data) => {
+    writeIntoFile(`./${DIR_PATH}/${name}.csv`, data);
+};
+
+const clearPastData = () => {
+  try {
+    fs.readdirSync(DIR_PATH).forEach(file =>
+      fs.unlinkSync(path.join(DIR_PATH, file)),
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const downloadData = () => {
+  clearPastData(); // clearing all data at first
+
+  // downloading data
+  Object.keys(symbolList).forEach(categoryName => {
+    const symbolsInfo = symbolList[categoryName];
+    symbolsInfo.forEach(({ name, symbol }) => {
+      const url = `https://query1.finance.yahoo.com/v7/finance/download/${symbol}?interval=1d&range=max`;
+      https.get(url, (res) => {
+        res.on('data', onDataReciveOf(name));
+      })
+        .on('error', onErrorReciveOf(name));
+    });
+  });
+};
+
+downloadData();
+
+
+
