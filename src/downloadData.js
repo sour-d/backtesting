df131339b29d3835f7124284ef56e1b7110a0fd0
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2023-07-06 12:02:36 +0530

    #5 Used Axios library

--- src/downloadData.js
+++ src/downloadData.js
@@ -1,8 +1,7 @@
 const symbolList = require('../symbolList.json');
-const https = require('node:https');
+const axios = require('axios');
 const fs = require('node:fs');
 const path = require('node:path');
-
 const DIR_PATH = './data';
 
 const writeIntoFile = (filename, data) => {
@@ -16,10 +15,7 @@ const onErrorReceiveOf = (name) =>
     console.error("Error is:", error);
   };
 
-const onDataReceiveOf = (name) =>
-  (data) => {
-    writeIntoFile(`./${DIR_PATH}/${name}.csv`, data);
-};
+const onDataReceiveOf = (name, data) => writeIntoFile(`./${DIR_PATH}/${name}.csv`, data);
 
 const clearPastData = () => {
   try {
@@ -38,18 +34,11 @@ const getUNIXTime = (date) => {
   return Math.round(Date.now() / 1000);
 };
 
-const getEndPoint = (symbol) => {
+const downloadData = () => {
+  const url = 'https://query1.finance.yahoo.com/v7/finance/download/';
   const period1 = getUNIXTime(new Date("2005-01-01"));
   const period2 = getUNIXTime();
 
-  const hostname = `https://query1.finance.yahoo.com`;
-  const url = `/v7/finance/download/${symbol}`;
-  const query = `?interval=1d&period1=${period1}&period2=${period2}`;
-
-  return hostname + url + query;
-};
-
-const downloadData = () => {
   console.log("Clearing past data...");
   clearPastData();
 
@@ -58,15 +47,17 @@ const downloadData = () => {
     const categorySymbolList = symbolList[categoryName];
 
     categorySymbolList.forEach(({ name, symbol }, index) => {
-      https.get(getEndPoint(symbol), (res) => {
-        res.on('data', onDataReceiveOf(name));
-        res.on('end', () => console.log(`${name} download complete`));
-      }).on('error', onErrorReceiveOf(name));
+      axios.get(`${url}${symbol}`, {
+        params: {
+          interval: "1d",
+          period1: period1,
+          period2: period2
+      }})
+        .then(res => onDataReceiveOf(name, res.data))
+        .catch(res => onErrorReceiveOf(name))
+        .finally(res => console.log(`${name} download complete`));
     });
   });
 };
 
 downloadData();
-
-
-
