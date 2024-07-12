d013b95d91eb31f048d8fae990f4c4ed42765de4
--global <36912347+sour-d@users.noreply.github.com>
2024-07-12 18:33:25 +0530

    fixed ping error

--- src/server/ping.js
+++ src/server/ping.js
@@ -5,7 +5,7 @@ const pingWebsite = async (url) => {
   if (!fs.existsSync("ping-log.txt"))
     fs.writeFileSync(
       "ping-log.txt",
-      JSON.stringify(`{start: ${new Date().toISOString()}}`)
+      JSON.stringify({ start: new Date().toISOString() })
     );
 
   const content = JSON.parse(fs.readFileSync("ping-log.txt", "utf8"));
